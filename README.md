ae734a9dac29066cccad0e6e543ae50c2f92a3e9
Jacob <skittlemann@gmail.com>
2021-10-31 17:00:24 -0500

    Commit frontend

---
+++ README.md
@@ -0,0 +1,1 @@
+https://trello.com/b/IM7Z0KEg/whichepisode-web
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div>Which Episode?</div>
+      <div>Allows you to pick a random episode from a show of your choice</div>
+    </div>
+  );
+}
+
+export default App;
