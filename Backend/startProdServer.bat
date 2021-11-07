58d81f83ada341f5183d980e8ee15748490e480f
Jacob <skittlemann@gmail.com>
2021-11-07 14:02:27 -0600

    Update debug launch script, add prod launch script

--- Backend/startProdServer.bat
+++ Backend/startProdServer.bat
@@ -1,7 +1,6 @@
 @echo off
 
 if defined VIRTUAL_ENV ( 
-    set DJANGO_DEBUG=1
     python ".\whichepisode_web\manage.py" runserver
     
 ) else ( echo ERROR: Not in virtual environment. Run startVenv.bat before running)
