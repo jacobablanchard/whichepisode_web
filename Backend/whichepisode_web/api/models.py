e2556597aa524aeb0f9c54046a5e0af0d24b6b10
Jacob <skittlemann@gmail.com>
2021-11-07 13:59:49 -0600

    Add a config database entry

--- Backend/whichepisode_web/api/models.py
+++ Backend/whichepisode_web/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TheMovieDBConfiguration(models.Model):
+    smallestImageSize = models.TextField()
+    baseURL = models.TextField()
+    dateRetrieved = models.DateTimeField()
