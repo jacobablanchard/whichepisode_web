be706a9d06d41c9fe88329cc266f8ebfe40eca65
Jacob <skittlemann@gmail.com>
2021-11-19 23:48:50 -0600

    Add a backdrop to the selected show

--- frontend/src/Components/ResultView/ResultView.tsx
+++ frontend/src/Components/ResultView/ResultView.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
 import { TVSearchResult } from "../../Classes/TVSearchResult";
+import { ResolvedURL } from "../../Classes/ResolvedURL";
+import axios from "axios";
 import "./ResultView.css";
 
 export interface IResultViewProps {
   show: TVSearchResult;
 }
 
-export interface IResultViewState {}
+export interface IResultViewState {
+  bannerURL: string | null;
+}
 
 export default class ResultView extends React.Component<
   IResultViewProps,
@@ -15,13 +19,43 @@ export default class ResultView extends React.Component<
   constructor(props: IResultViewProps) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      bannerURL: null,
+    };
+  }
+
+  async componentDidMount() {
+    //this.updateImageURL();
+  }
+
+  async componentDidUpdate(prevProps: IResultViewProps) {
+    if (prevProps.show.backdrop_path !== this.props.show.backdrop_path)
+      this.updateImageURL();
+  }
+
+  private async updateImageURL() {
+    if (this.props.show.backdrop_path === null) {
+      return;
+    }
+    const url =
+      process.env.NODE_ENV === "development"
+        ? `http://localhost:8000/api/lookup/backdrop_url${this.props.show.backdrop_path}`
+        : `http://localhost:8000/api/lookup/backdrop_url${this.props.show.backdrop_path}`;
+    const { data } = await axios.get<ResolvedURL>(encodeURI(url));
+    this.setState({
+      bannerURL: data.resolvedURL,
+    });
   }
 
   public render() {
     return (
       <div className="resultView">
         <h1 className="movieTitle">{this.props.show.name}</h1>
+        <img
+          src={this.state.bannerURL === null ? "" : this.state.bannerURL}
+          alt="The banner image of the show"
+          className="backdropImage"
+        ></img>
       </div>
     );
   }
