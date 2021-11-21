7a95a24fc1800a019c5ef6e2271c04465650247d
Jacob <skittlemann@gmail.com>
2021-11-20 20:02:15 -0600

    Add SeriesInfo and Season interfaces in prep of adding more episode info

---
+++ frontend/src/Classes/SeriesInfo.tsx
@@ -0,0 +1,35 @@
+import { Season } from "./Season";
+
+export interface SeriesInfo {
+  backdrop_path: string;
+  //created_by:           CreatedBy[];
+  episode_run_time: number[];
+  first_air_date: Date;
+  //genres:               Genre[];
+  homepage: string;
+  id: number;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: Date;
+  //last_episode_to_air:  LastEpisodeToAir;
+  name: string;
+  next_episode_to_air: null;
+  //networks:             Network[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: any[];
+  // production_countries: ProductionCountry[];
+  seasons: Season[];
+  // spoken_languages:     SpokenLanguage[];
+  status: string;
+  tagline: string;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+}
